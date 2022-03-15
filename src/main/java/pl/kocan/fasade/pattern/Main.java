da6ec94b20918b97196c2cba86b79e6784c08cdc
Paweł Kocan <pkocan@onet.pl>
2022-03-15 22:14:18 +0100

    Facade pattern implementation

--- src/main/java/pl/kocan/fasade/pattern/Main.java
+++ src/main/java/pl/kocan/fasade/pattern/Main.java
@@ -1,23 +1,12 @@
 package pl.kocan.fasade.pattern;
 
-import pl.kocan.fasade.pattern.delivery.DeliveryBox;
-import pl.kocan.fasade.pattern.delivery.DeliveryBoxSystem;
+import pl.kocan.fasade.pattern.delivery.DeliveryBoxFacade;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        pickupPackage();
-    }
-
-    private static void pickupPackage() {
-
-        DeliveryBox deliveryBox = new DeliveryBox();
-        DeliveryBoxSystem deliveryBoxSystem = new DeliveryBoxSystem();
-
-        deliveryBox.getUserData();
-        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
-            deliveryBox.openBox();
-        }
+        DeliveryBoxFacade deliveryBoxFacade = new DeliveryBoxFacade();
+        deliveryBoxFacade.pickupPackage();
     }
 }
